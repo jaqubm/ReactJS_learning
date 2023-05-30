1f99237bc6f3bcc594f86db30d94635b1a2ef5a8
jaqubm <jaqobgm@gmail.com>
2023-05-30 16:08:55 +0200

    Updated Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ styles }) => (
-  <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+  <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-xl ${styles}`}>
     Get Started
   </button>
 )
