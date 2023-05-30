2643d1558b705521363c69945c538b57932a2cd1
jaqubm <jaqobgm@gmail.com>
2023-05-30 20:36:39 +0200

    Small updates in components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      <a href='https://jaqubm.dev/'>
+        <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      </a>
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
